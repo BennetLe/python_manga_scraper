16e7ffec99944c24b8dd703e17128647f1a56b16
Bennet <bennetleins@gmx.de>
2022-03-11 08:17:03 +0100

    Working scraper for all the Manga Links

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ page = requests.get(URL)
 links_manga = []
 name_chapter = []
 
+def main():
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="ResultsContainer")
+    results_class = soup.find_all("a", class_="chapter-name text-nowrap")
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
-results_class = soup.find_all("a", class_="chapter-name text-nowrap")
+    for result_class in reversed(results_class):
+        link = result_class["href"]
+        name = result_class.text.strip()
+        print(name + ":\n")
+        print(link, end="\n" * 2)
+        links_manga.append(link)
+        name_chapter.append(name)
 
-for result_class in reversed(results_class):
-    link = result_class["href"]
-    name = result_class.text.strip()
-    print(name+":\n")
-    print(link, end="\n" * 2)
-    links_manga.append(link)
-    name_chapter.append(name)
+    for link in links_manga:
+        print(link + "\n")
 
 
-for link in links_manga:
-    print(link+"\n")
+if __name__ == '__main__':
+    main()

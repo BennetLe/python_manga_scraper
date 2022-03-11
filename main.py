bd06cdbe9f3739aa4944df10929ee80a7b68eece
Bennet <bennetleins@gmx.de>
2022-03-11 16:09:43 +0100

    Working scraper for specified Manga from Manganato.com Deleting the old pictures now and fixed a bug Possible to wait after each Chapter(commented out)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 URL = "https://readmanganato.com/manga-iw985579"
 page = requests.get(URL)
@@ -14,7 +17,7 @@ file_path = []
 
 
 def main():
-    chaptername = 0
+    chapter_name = 0
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(id="ResultsContainer")
     results_class = soup.find_all("a", class_="chapter-name text-nowrap")
@@ -43,7 +46,7 @@ def main():
             s.headers.update({"referer": "https://readmanganato.com/"})
             picture = s.get(chapter_picture, verify=False)
 
-            if chapter.has_key("alt"):
+            if chapter.has_attr("alt"):
                 filename = chapter["alt"]
             else:
                 filename = "BackButton"
@@ -61,17 +64,23 @@ def main():
             Image.open(f)
             for f in file_path
         ]
-        chaptername += 1
+        chapter_name += 1
 
         for i in range(0, len(images)):
             if images[i].mode == 'RGBA':
                 images[i] = images[i].convert('RGB')
 
-        pdf_path = f"/Users/benne/Pictures/Manga_PDF/Chapter_{chaptername}.pdf"
+        pdf_path = f"/Users/benne/Pictures/Manga_PDF/Chapter_{chapter_name}.pdf"
 
         images[0].save(
             pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
         )
+        for file in glob.glob("*.png"):
+            os.remove(file)
+
+        print(f"[*] Chapter {chapter_name} is converted to PDF")
+        # input("Press Enter to continue...")
+        images.clear()
 
 
 if __name__ == '__main__':

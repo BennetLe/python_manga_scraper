8b23d32a7520a49d9e2933b8f5e406ce5ac1f0d3
Bennet <bennetleins@gmx.de>
2022-03-18 16:05:27 +0100

    Working scraper for specified Manga from Manganato.com Simple GUI added

--- main.py
+++ main.py
@@ -16,20 +16,23 @@ def main():
         if URL != "" and path != "":
             all_chapters.get(URL, path)
 
-    window.rowconfigure([0, 1, 2, 3], minsize=25, weight=1)
+    window.rowconfigure([0, 1, 2, 3, 4], minsize=25, weight=1)
     window.columnconfigure([0], minsize=300, weight=1)
 
+    input_lable = tk.Label(master=window, text="Enter the Manganato.com link")
+    input_lable.grid(row=0, column=0, sticky="nsew")
+
     input_URL = tk.Entry(master=window)
-    input_URL.grid(row=0, column=0, sticky="nsew")
+    input_URL.grid(row=1, column=0, sticky="nsew")
 
     btn_path = tk.Button(master=window, text="Open path selector", command=set_path)
-    btn_path.grid(row=1, column=0, sticky="nsew")
+    btn_path.grid(row=2, column=0, sticky="nsew")
 
     lbl_path = tk.Label(master=window, text="")
     # lbl_path.grid(row=2, column=0, sticky="nsew")
 
     btn_submit = tk.Button(master=window, text="Get Manga", command=get_manga)
-    btn_submit.grid(row=3, column=0, sticky="nsew")
+    btn_submit.grid(row=4, column=0, sticky="nsew")
 
     window.mainloop()
 
